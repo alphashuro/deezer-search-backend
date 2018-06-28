b5f72e8b93a0b681b928b7359dc524c201ea6f06
Alpha Shuro <alphashuro@gmail.com>
2018-06-29 00:09:44 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+const Koa = require("koa");
+const Router = require("koa-router");
+const logger = require("koa-logger");
+const cors = require("@koa/cors");
+const request = require("request-promise-native");
+const querystring = require("querystring");
+
+const credentials = {
+  client: {
+    id: process.env.DEEZER_CLIENT_ID,
+    secret: process.env.DEEZER_CLIENT_SECRET
+  },
+  auth: {
+    tokenHost: "https://connect.deezer.com/oauth/auth.php"
+  }
+};
+
+const router = new Router();
+
+router.get("/auth", ctx => {
+  const appId = process.env.DEEZER_APP_ID;
+  const redirect = encodeURIComponent(process.env.DEEZER_REDIRECT);
+  const perms = "basic_access";
+
+  ctx.redirect(
+    `https://connect.deezer.com/oauth/auth.php?app_id=${appId}&redirect_uri=${redirect}&perms=${perms}`
+  );
+});
+
+router.get("/callback", async ctx => {
+  const appId = process.env.DEEZER_APP_ID;
+  const appSecret = process.env.DEEZER_APP_SECRET;
+  const { code } = ctx.query;
+
+  if (!code) {
+    return (ctx.status = 401);
+  }
+
+  const url = `https://connect.deezer.com/oauth/access_token.php?app_id=${appId}&secret=${appSecret}&code=${code}`;
+  const result = querystring.parse(await request(url));
+
+  ctx.body = result.access_token;
+});
+
+const app = new Koa();
+
+app.use(logger());
+app.use(cors());
+app.use(router.middleware());
+
+app.listen(process.env.PORT || 8080);
